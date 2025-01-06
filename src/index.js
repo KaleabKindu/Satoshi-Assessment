6f52a2d51ec82acc2316e887143d1015a75a1de1
KaleabKindu <kkmichaelstarkk@gmail.com>
2025-01-06 13:20:57 +0300

    setup react with webpack

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+// Get the DOM element where you want to mount the app
+const rootElement = document.getElementById('root');
+// Create a root
+const root = createRoot(rootElement);
+
+// Render your app
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
